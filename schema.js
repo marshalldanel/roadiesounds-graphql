857b829a3436108c4ae8c6cc66c4eeb6817d5295
Marshall Danel <marshalldanel@gmail.com>
2017-10-13 23:58:46 -0700

    Update to make spotify api call, logs artist id yet fails to log artist tracks after making call

--- schema.js
+++ schema.js
@@ -8,6 +8,58 @@ const {
   GraphQLList
 } = require('graphql');
 
+// Spotify //
+
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: client_id,
+  clientSecret: client_secret,
+});
+
+// // 
+
+const TracksType = new GraphQLObjectType({
+  name: 'Tracks',
+  description: '...',
+
+  fields: () => ({
+    name: {
+      type: GraphQLString,
+      resolve: response => {
+        console.log('this is not working right now ;(')
+      }
+    }
+  })
+})
+
+const SpotifyType = new GraphQLObjectType({
+  name: 'Artist',
+  description: '...',
+
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+      resolve: response => {
+        return response.artists.items[0].id
+      }
+    },
+    tracks: {
+      type: new GraphQLList(TracksType),
+      resolve: response => {
+        const id = response.artists.items[0].id
+        return fetch(
+          `https://api.spotify.com/v1/artists/${id}/top-tracks?country=US&access_token=${process.env.ACCESS_TOKEN}`
+        )
+        .then(response => response.json())
+      }
+    }
+  })
+})
+
 const EventType = new GraphQLObjectType({
   name: 'Event',
   description: '...',
@@ -17,9 +69,6 @@ const EventType = new GraphQLObjectType({
       type: GraphQLString,
       resolve: event =>
       event.city_name
-    },
-    title: {
-      type: GraphQLString
     },
     date: {
       type: GraphQLString,
@@ -46,15 +95,28 @@ const EventType = new GraphQLObjectType({
       resolve: event =>
         event.performers.performer.url
     },
-    venue_name: {
-      type: GraphQLString,
-      resolve: event =>
-        event.venue_name
+    title: {
+      type: GraphQLString
+    },
+    spotify: {
+      type: SpotifyType,
+      resolve: event => {
+        const artist = event.performers.performer.name;
+        return fetch(
+          `https://api.spotify.com/v1/search?q=${artist}&type=artist&access_token=${process.env.ACCESS_TOKEN}&limit=1`
+        )
+        .then(response => response.json())
+      }
     },
     venue_address: {
       type: GraphQLString,
       resolve: event =>
         event.venue_address
+    },
+    venue_name: {
+      type: GraphQLString,
+      resolve: event =>
+        event.venue_name
     },
     venue_url: {
       type: GraphQLString,
@@ -100,8 +162,22 @@ module.exports = new GraphQLSchema({
         },
         resolve: (root, args) => fetch(
             `http://api.eventful.com/json/events/search?app_key=${process.env.EVENTFUL_KEY}&categories=music_${args.genre}&location=${args.location}&date=${args.start_date}00-${args.end_date}00&page_size=25`
-          )
+        )
           .then(response => response.json())
+      },
+      // Only add to get an access token if expires
+      spotify: {
+        type: GraphQLString,
+        resolve: () => {
+          spotifyApi.clientCredentialsGrant()
+          .then((data) => {
+            spotifyApi.setAccessToken(data.body.access_token);
+            console.log('our spotify info is: ', spotifyApi);
+          }, (err) => {
+            console.log('something went wrong :( >>> ', err);
+            process.exit(-1); // if we don't have a working spotify credentials grant, all is lost.
+          });
+        }
       }
     })
   })
